7c72ddf842d70072a6f3c08b1eb719b15178e814
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-05-09 00:22:10 +0300

    Add project files.

---
+++ PratikIf_Else/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Bir sayı giriniz.");
+
+int sayi = Convert.ToInt32(Console.ReadLine());
+int bolumdenKalan = sayi % 2;
+
+if (sayi == 10)
+{
+    Console.WriteLine("Girilen sayı 10'a eşittir.");
+}
+else if (sayi < 10)
+{
+    Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+}
+if (bolumdenKalan == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir.");
+}
